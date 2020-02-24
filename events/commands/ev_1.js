b9a044e75e05b94dd17c865ba0078adce91cf9d4
damiancipolat <damian.cipolat@gmail.com>
2020-02-24 17:54:50 -0300

    add

--- events/commands/ev_1.js
+++ events/commands/ev_1.js
@@ -2,6 +2,12 @@ const {
   getResource
 } = require('../../service/mock.js');
 
+const {
+  EVENT_1,
+  EVENT_1_SUCCESS,
+  EVENT_1_FAIL
+} = require('../dictionary.js')
+
 const uuidv1 = require('uuid/v1');
 
 //Process the event, if is success emit SUCCESS is error emit ERROR.
@@ -9,11 +15,30 @@ const listener = (emitter) => async (payload) => {
 
   try{
 
-    const result = await getResource();
-    console.log('xxxx',result);
+    console.log('Received event',EVENT_1);
+
+    //Make the request.
+    const response = await getResource();
+
+    //Generate uuid.
+    const uuid = uuidv1();
+    
+    //Make response.
+    const payload = {
+      uuid,
+      response
+    };
+
+    console.log('Emit event -->',EVENT_1_SUCCESS, 'Payload',payload);
+    emitter.emit(EVENT_1_SUCCESS, payload)
 
   } catch(err){
-    console.log('Error',err);
+
+    console.log('Emit event -->',EVENT_1_FAIL);
+    console.error('Error',err);
+
+    emitter.emit(EVENT_1_FAIL,{error:err});
+
   }
 
  }
